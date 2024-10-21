e4aa3606ebfd1a4fb368e48f9d5e69b1742cc270
Vivek Raman <vivek-raman@outlook.com>
2024-10-20 21:56:14 -0700

    Init project

---
+++ Assets/Scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
